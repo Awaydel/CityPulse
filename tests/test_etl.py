828dfd7b869f3dcc08f2f726a4ef247d1f0aad43
Андрей <qtreyand@mail.ru>
2025-12-16 17:03:34 +0400

    ci: Фикс импортов №4 (sys.path hack)
    - Явное добавление корневой директории в sys.path внутри теста
    - Это должно исправить ошибку ModuleNotFoundError в GitHub Actions окончательно

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Explicitly add the project root to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 import pandas as pd
 import numpy as np
